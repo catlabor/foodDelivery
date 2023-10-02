c4828072f65f9d68df8906b52e7b02d17d0e89d2
catlabor <a972500794@gmail.com>
2023-10-02 22:33:44 +0900

    update1002

--- food_delivery/src/test/java/com/start/food_delivery/FoodDeliveryApplicationTests.java
+++ food_delivery/src/test/java/com/start/food_delivery/FoodDeliveryApplicationTests.java
@@ -1,9 +1,14 @@
 package com.start.food_delivery;
 
+import com.food.order.foodApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+
+
+@SpringBootTest(classes = foodApplication.class)
 class FoodDeliveryApplicationTests {
 
     @Test
